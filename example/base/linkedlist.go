884f3eb2e5d366085f1b8329626f0cec1dc8c068
Rickh <hpx210650@gmail.com>
2021-05-29 21:39:15 +0800

    update

---
+++ example/base/linkedlist.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	Val uint16;
+	Next *Node;
+}
+
+func main() {
+	// start := new(Node);
+	
+
+	fmt.Println("aaa");
+}
+
+func appendNode() {
+
+}
